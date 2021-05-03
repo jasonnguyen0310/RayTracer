db03207a0b879db4994094386fe75f8af982ea80
jasonnguyen0310 <jasonnguyen0310@gmail.com>
2021-05-03 02:41:19 -0700

    Directory holds Sphere class that represents the scene

---
+++ scene/Sphere.hpp
@@ -0,0 +1,24 @@
+/*
+Sphere.hpp
+Author: Jason Nguyen
+May 2, 2021
+*/
+
+#include "../geometry/Point.hpp"
+#include "../geometry/Ray.hpp"
+#include "../geometry/Vector.hpp"
+
+#ifndef SPHERE_H
+#define SPHERE_H
+
+class Sphere
+{
+    public:
+        Point center;
+        int radius;
+        Sphere();
+        Sphere(Point c, int r) : center(c), radius(r) {}
+        Vector intersections(Ray r);
+};
+
+#endif /* SPHERE_H */
