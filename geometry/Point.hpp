a0907447b08099411763712b6ea4ac29effd4977
jasonnguyen0310 <jasonnguyen0310@gmail.com>
2021-05-03 02:39:08 -0700

    Point Struct: represents a point in 3D space

--- geometry/Point.hpp
+++ geometry/Point.hpp
@@ -4,13 +4,17 @@ Author: Jason Nguyen
 May 3, 2021
 */
 
+#include "Vector.hpp"
+
 #ifndef POINT_H
 #define POINT_H
+
 struct Point
 {
     double x;
     double y;
     double z;
+    Point();
     Point(double x_coord, double y_coord, double z_coord) : x(x_coord), y(y_coord), z(z_coord) {}
 };
 
