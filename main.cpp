6be3512974e495094a413fcb22b776030cedc467
jasonnguyen0310 <jasonnguyen0310@gmail.com>
2021-05-13 14:53:47 -0700

    fix

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main(int, char *argv[]) {
     Scene scene{argv[1]};
     int resolution = static_cast<int>(strtol(argv[2], nullptr, 10));
-    Raytracer raytracer{move(scene)};
+    Raytracer raytracer{scene};
     PGMOut().save(raytracer.to_raster(resolution));
     return 0;
 }
